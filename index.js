9e2006b6255ce9a39559e582d94828977a6d408d
Bocong Zhao <zhaobocong@hotmail.com>
2019-03-14 15:48:45 -0400

    Supported ES6 and added endpoint

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-// const express = require("express");
+import express from 'express';
+import https from 'https';
+import axios from 'axios';
 
-// const app = new express();
-const https = require("https");
-const axios = require("axios");
+const app = new express();
+const PORT = process.env.PORT || 5000;
 
 const agent = new https.Agent({
   rejectUnauthorized: false
 });
-axios
-  .get("https://192.168.92.227:11443/ext/xbox/info", { httpsAgent: agent })
-  .then(response => console.log(response.data));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/api', (req, res) => {
+  const { url } = req.body;
+  axios.get(url, { httpsAgent: agent }).then(response => response.data);
+});
+
+app.listen(PORT, () => console.log(`app is running on port: ${PORT}`));
